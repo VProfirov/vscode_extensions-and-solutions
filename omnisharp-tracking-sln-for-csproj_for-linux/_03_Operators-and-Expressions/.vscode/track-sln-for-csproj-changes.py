aa958be1e22fcaabe900cae0b1111894b23cf0fa
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-03-19 22:23:02 +0200

    0.0001

--- omnisharp-tracking-sln-for-csproj_for-linux/_03_Operators-and-Expressions/.vscode/track-sln-for-csproj-changes.py
+++ omnisharp-tracking-sln-for-csproj_for-linux/_03_Operators-and-Expressions/.vscode/track-sln-for-csproj-changes.py
@@ -1,6 +1,10 @@
 #current suggested path localdir/.vscode/*.py
 
 from pathlib import Path
+import pathlib
+import os
+# with open(Path(__file__).parent.parent/str('*.sln'),mode='r')
+
 
 print(f'-----.sln------')
 
@@ -12,12 +16,21 @@ script_parent_dir = Path(script_file_path).parent
 sln_parent_dir = Path(script_parent_dir).parent
 
 print(f'--> {sln_parent_dir}')
+
+#files in current dir
+dirs_files = Path(sln_parent_dir).iterdir()
+
+print(f'--> --> {dirs_files}')
+print(f'--> os --> {os.getcwd()}')
+
 # print(f'--> {sln_file_dir.parent.parent}')
+filestream = open(sln_parent_dir/'_03_Operators-and-Expressions.sln','r')
+print(f'--> {filestream}')
 
-
+# s1 = filestream.
+content_file = filestream.read()
+print(f'--> {content_file}')
 # print(get_project_names_from_sln())
 print(f'------end-------')
-
-
 
 #DOCUMENATION on PATH handling: https://realpython.com/python-pathlib/
